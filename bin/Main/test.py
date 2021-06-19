ea4e306c6391e7876a31632e5c1d63dc816feaaf
Andrzej <andstr2@st.amu.edu.pl>
2021-06-19 23:20:17 +0200

    Neural networks learning

--- bin/Main/test.py
+++ bin/Main/test.py
@@ -1,15 +1,2 @@
-import pandas as pd
-
-
-def main():
-    my_dict = {'known': [1], 'power': [1], 'new': [1], 'location': 0, 'stable': [1], 'chain_reaction': [1]}
-
-    s = pd.DataFrame.from_dict(my_dict)
-
-    print(s)
-
-    # data = {'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}
-    # print(pd.DataFrame.from_dict(data))
-
-
-main()
+t = range(10)
+print(t)
